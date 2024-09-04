9d9ea08b056e79d7dd4ef780e11a59005b8adf30
Imbajanox <33042034+Imbajanox@users.noreply.github.com>
2024-09-04 19:05:41 +0200

    add win patterns

--- script.js
+++ script.js
@@ -44,7 +44,11 @@ function gameController (player1, player2, board) {
     };
 
     const checkWin = () => {
-
+        const winPatterns = [
+            [0, 1, 2], [3, 4, 5], [6, 7, 8],
+            [0, 3, 6], [1, 4, 7], [2, 5, 8],
+            [0, 4, 8], [2, 4, 6]
+        ];
     }
 
     const getCurrentPlayer = () => currentPlayer;
