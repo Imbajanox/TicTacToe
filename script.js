649ac468ca5f05a1ab6a8a2f43a63674dd47cf31
Imbajanox <33042034+Imbajanox@users.noreply.github.com>
2024-09-10 20:47:55 +0200

    add Score functionality

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-function createPlayer (name, marker) {
-    return { name, marker };
+function createPlayer (name, marker, score) {
+    return { name, marker, score };
 }
 
 const board = (function gameboard () {
@@ -41,7 +41,8 @@ function gameController (player1, player2, board) {
 
 
             if(checkWin()) {
-                return `${currentPlayer.name} wins!`
+                currentPlayer.score += 10;
+                return `${currentPlayer.name} wins!`;
             } else if (board.getBoard().every(cell => cell !== '')) {
                 return "It's a draw!";
             }
@@ -74,7 +75,7 @@ function gameController (player1, player2, board) {
 
 //AI
 
-const AIPlayer = (marker) => {
+const AIPlayer = (marker, score) => {
     const getMove = (board) => {
         let availableMoves = board.getBoard().map((cell, index) => cell === '' ? index : null).filter(index => index !== null);
         if(availableMoves.length > 0){
@@ -83,20 +84,21 @@ const AIPlayer = (marker) => {
         return null;
     };
 
-    return {marker, getMove};
+    return {marker, score, getMove};
 };
 
 
 //Test Game
 
-const player1 = createPlayer('Player 1', 'X');
-const aiPlayer = AIPlayer('O');
+const player1 = createPlayer('Player 1', 'X', 0);
+const aiPlayer = AIPlayer('O', 0);
 
 const game = gameController(player1, aiPlayer, board);
 
 const playWithAI = (index) => {
     let result = game.playRound(index);
     updateUI();
+    setScores(player1.score, aiPlayer.score);
     if (result) {
         for (let i = 0; i < board.getBoard().length; i++) {
             console.log(board.getBoard()[i]);
@@ -110,7 +112,7 @@ const playWithAI = (index) => {
     let aiMove = aiPlayer.getMove(board);
     result = game.playRound(aiMove);
     updateUI();
-
+    setScores(player1.score, aiPlayer.score);
     if (result) {
         console.log(result);
         //alert(result);
@@ -121,6 +123,14 @@ const playWithAI = (index) => {
 };
 
 //UI
+
+const setScores = (score1, score2) => {
+    const playerScore = document.querySelector('.playerScore');
+    const aiScore = document.querySelector('.aiScore');
+
+    playerScore.textContent = `Player: ${score1} Points`;
+    aiScore.textContent = `AI: ${score2} Points`;
+}
 
 const updateUI = () => {
     const cells = document.querySelectorAll('.cell');
